758fec934e55d360e391f4f81607531aefa398f4
ferreira <ferreira@boom.ai>
2024-11-05 17:16:51 -0600

    fix

--- nubeFinal/nubeFinal/urls.py
+++ nubeFinal/nubeFinal/urls.py
@@ -1,7 +1,7 @@
 # nubeFinal/urls.py
 from django.contrib import admin
 from django.urls import path
-from . import views
+from APP import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
